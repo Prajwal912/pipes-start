86412037b97f1236164c2b0fc99457b1e473e603
prajwal <prajwalshah912@gmail.com>
2023-07-23 19:56:16 +0530

    added another custom pipe

---
+++ src/app/first-letter.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'firstLetter'
+})
+export class FirstLetterPipe implements PipeTransform {
+  transform(value: any): any {
+  return   value.charAt(0).toUpperCase() + value.slice(1);
+   }
+
+}
--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShortenPipe implements PipeTransform {
 
   transform(value: any, ...args: unknown[]): any {
-   value.charAt(0).toUpperCase() + value.slice(1);
+     if(value.length>10){
+   return  value.substr(0,10) + '...'
+     }
+
   }
 
 }
